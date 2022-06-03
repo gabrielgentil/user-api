99c9e3e8581a720e42a0c8fe82901d50bb0670e7
gabrielgentil <gabriel.sgentil97@gmail.com>
2022-06-02 22:31:58 -0300

    feat: implements InMemoryUserRepository

--- src/usecases/register-user-on-mailing-list/repository/in-memory-register-user-on-mailing-list.ts
+++ src/usecases/register-user-on-mailing-list/repository/in-memory-register-user-on-mailing-list.ts
@@ -2,25 +2,25 @@ import { UserRepository } from '../../ports/user-repository'
 import { UserData } from '../user-data'
 
 export class InMemoryUserRepository implements UserRepository {
-  private repository: UserData[]
+	private repository: UserData[]
 
-  constructor (repository: UserData[]) {
-    this.repository = repository
-  }
+	constructor (repository: UserData[]) {
+		this.repository = repository
+	}
 
-  add (user: UserData): Promise<void> {
-    throw new Error('Method not implemented.')
-  }
+	add (user: UserData): Promise<void> {
+		throw new Error('Method not implemented.')
+	}
 
-  findByEmail (email: string): Promise<UserData> {
-    return null
-  }
+	findByEmail (email: string): Promise<UserData> {
+		return null
+	}
 
-  findAllUsers (): Promise<UserData[]> {
-    throw new Error('Method not implemented.')
-  }
+	findAllUsers (): Promise<UserData[]> {
+		throw new Error('Method not implemented.')
+	}
 
-  exists (user: UserData): Promise<boolean> {
-    throw new Error('Method not implemented.')
-  }
+	exists (user: UserData): Promise<boolean> {
+		throw new Error('Method not implemented.')
+	}
 }
