8ea555f9cfa706b413d3463051fde38f4407d4ad
gabrielgentil <gabriel.sgentil97@gmail.com>
2022-06-02 22:47:14 -0300

    test: add and find user and checks its existence

--- test/src/usecases/register-user-on-mailing-list/repository/in-memory-register-user-on-mailing-list.spec.ts
+++ test/src/usecases/register-user-on-mailing-list/repository/in-memory-register-user-on-mailing-list.spec.ts
@@ -10,4 +10,14 @@ describe('In memory User repository', () => {
 
 		expect(user).toBeNull()
 	})
+
+	test('should return user if it is found in the repository', async () => {
+		const users: UserData[] = []
+		const name = 'any_name'
+		const email = 'any@email.com'
+		const userRepo = new InMemoryUserRepository(users)
+		await userRepo.add({ name, email })
+		const user = await userRepo.findByEmail('any@email.com')
+		expect(user.name).toBe('any_name')
+	})
 })
