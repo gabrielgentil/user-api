d4c388e55738c46ec527a910a835cdad1d10b090
gabrielgentil <gabriel.sgentil97@gmail.com>
2022-06-02 22:28:41 -0300

    test: implements InMemoryUserRepository

---
+++ test/src/usecases/register-user-on-mailing-list/repository/in-memory-register-user-on-mailing-list.spec.ts
@@ -0,0 +1,13 @@
+import { UserData } from '../../../../../src/usecases/register-user-on-mailing-list/user-data'
+import { InMemoryUserRepository } from '../../../../../src/usecases/register-user-on-mailing-list/repository/in-memory-register-user-on-mailing-list'
+
+describe('In memory User repository', () => {
+  test('should return null if user is not found', async () => {
+    const users: UserData[] = []
+    const userRepo = new InMemoryUserRepository(users)
+
+    const user = await userRepo.findByEmail('any@email.com')
+
+    expect(user).toBeNull()
+  })
+})
