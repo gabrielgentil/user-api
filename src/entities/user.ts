6c6a264d3c9aa325febfa45098799d8160e6b213
gabrielgentil <gabriel.sgentil97@gmail.com>
2022-06-05 23:03:39 -0300

    feat: verify name while creating user

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -1,10 +1,20 @@
 import { Either, left } from '../shared/either'
 import { Email } from './email'
 import { InvalidEmailError } from './errors/invalid-email'
+import { InvalidNameError } from './errors/invalid-name'
+import { Name } from './name'
 import { UserData } from './user-data'
 
 export class User {
-	static create (userData: UserData): Either<InvalidEmailError, User> {
+	static create (
+		userData: UserData
+	): Either<InvalidNameError | InvalidEmailError, User> {
+		const nameOrError = Name.create(String(userData.name))
+
+		if (nameOrError.isLeft()) {
+			return left(new InvalidNameError())
+		}
+
 		const emailOrError = Email.create(String(userData.email))
 
 		if (emailOrError.isLeft()) {
