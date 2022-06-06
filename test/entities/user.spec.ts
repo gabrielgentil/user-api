402f6df455fd0e1d0c528ee6dcf22474d36eef65
gabrielgentil <gabriel.sgentil97@gmail.com>
2022-06-05 23:03:57 -0300

    test: verify name while creating user

--- test/entities/user.spec.ts
+++ test/entities/user.spec.ts
@@ -1,4 +1,5 @@
 import { InvalidEmailError } from '../../src/entities/errors/invalid-email'
+import { InvalidNameError } from '../../src/entities/errors/invalid-name'
 import { User } from '../../src/entities/user'
 import { left } from '../../src/shared/either'
 
@@ -7,5 +8,17 @@ describe('User domain class', () => {
 		const invalidEmail = 'invalid_email'
 		const error = User.create({ name: 'any_name', email: invalidEmail })
 		expect(error).toEqual(left(new InvalidEmailError()))
+	})
+
+	test('should not create user with invalid name (too few characters)', () => {
+		const invalidName = 'a     '
+		const error = User.create({ name: invalidName, email: 'any@email.com' })
+		expect(error).toEqual(left(new InvalidNameError()))
+	})
+
+	test('should not create user with invalid name (too many characters)', () => {
+		const invalidName = 'a'.repeat(257)
+		const error = User.create({ name: invalidName, email: 'any@email.com' })
+		expect(error).toEqual(left(new InvalidNameError()))
 	})
 })
