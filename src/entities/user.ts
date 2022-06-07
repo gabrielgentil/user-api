daaaf89c43506386d0a3e65d60da7a571eb36f75
gabrielgentil <gabriel.sgentil97@gmail.com>
2022-06-07 20:17:50 -0300

    refactor: don't instantiate error twice

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -20,13 +20,13 @@ export class User {
 		const nameOrError = Name.create(String(userData.name))
 
 		if (nameOrError.isLeft()) {
-			return left(new InvalidNameError(String(userData.name)))
+			return left(nameOrError.value)
 		}
 
 		const emailOrError = Email.create(String(userData.email))
 
 		if (emailOrError.isLeft()) {
-			return left(new InvalidEmailError(String(userData.email)))
+			return left(emailOrError.value)
 		}
 
 		const name: Name = nameOrError.value
